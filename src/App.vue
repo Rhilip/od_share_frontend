0c638ffe17e72d8b196a36e166ba07bd0f32bf94
Rhilip <rhilipruan@gmail.com>
2020-05-18 20:07:20 +0800

    增加说明

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
         <img alt="Logo" src="./assets/logo.jpg" style="width: 200px;">
         <br>
         <h1> R酱的资源收纳库 </h1>
-        <span></span>
+        <span>此处资源均为软件全自动搬运至OneDrive国际版分享，没有条理且未经任何整理辨别！！！！</span>
+        <span>请善用搜索功能进行检索~</span>
 
         <Row type="flex">
             <Col span="20" offset="2">
